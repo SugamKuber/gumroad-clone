166b879c7c3387b8680c06d4bc3a2c21ee593546
sugamkuber <122772320+grovill@users.noreply.github.com>
2023-03-12 03:08:03 +0530

    feat: adding animation

--- src/components/End.js
+++ src/components/End.js
@@ -1,6 +1,27 @@
+import React, { useEffect } from "react";
+import { useAnimation, motion } from "framer-motion";
+import { useInView } from "react-intersection-observer";
+
 import Treeguy from "../assets/treeguy.svg";
 import Workguy from "../assets/workguy.svg";
+
+const squareVariants = {
+  visible: { opacity: 1, scale: 1, transition: { duration: 1 } },
+  hidden: { opacity: 0.1, scale: 0.7 },
+};
+const smallsquareVariants = {
+  visible: { opacity: 1, scale: 1, transition: { duration: 0.6 } },
+  hidden: { opacity: 0.1, scale: 0.6 },
+};
+
 function End() {
+  const controls = useAnimation();
+  const [ref, inView] = useInView();
+  useEffect(() => {
+    if (inView) {
+      controls.start("visible");
+    }
+  }, [controls, inView]);
   return (
     <>
       <div className=" grid grid-cols-2 divide-x">
@@ -17,16 +38,48 @@ function End() {
       </div>
       <div className=" grid grid-cols-2 divide-x">
         <div className="bg-[#000000] p-28">
-          <div className="absolute bg-white px-10 py-6 rounded-2xl font-semibold border-black border-2  -translate-x-8 -translate-y-7">
-            Instead of selling a book...
-          </div>
-          <img src={Workguy} alt="Work guy" />
+          <motion.div
+            ref={ref}
+            animate={controls}
+            initial="hidden"
+            variants={smallsquareVariants}
+            className="square z-30"
+          >
+            <div className=" z-30 absolute bg-white px-10 py-6 rounded-2xl font-semibold border-black border-2  -translate-x-8 -translate-y-7">
+              Instead of selling a book...
+            </div>
+          </motion.div>
+          <motion.div
+            ref={ref}
+            animate={controls}
+            initial="hidden"
+            variants={squareVariants}
+            className="square"
+          >
+            <img src={Workguy} alt="Work guy" className="-z-30" />
+          </motion.div>
         </div>
         <div className="bg-[#ffc900] p-28">
-          <div className="absolute bg-white px-10 py-6 rounded-2xl border-black border-2 font-semibold -translate-x-8 -translate-y-6">
-            ...start by selling a blog post!
-          </div>
-          <img src={Treeguy} alt="Tree guy" />
+          <motion.div
+            ref={ref}
+            animate={controls}
+            initial="hidden"
+            variants={smallsquareVariants}
+            className="square z-30"
+          >
+            <div className="absolute z-30 bg-white px-10 py-6 rounded-2xl border-black border-2 font-semibold -translate-x-8 -translate-y-6">
+              ...start by selling a blog post!
+            </div>
+          </motion.div>
+          <motion.div
+            ref={ref}
+            animate={controls}
+            initial="hidden"
+            variants={squareVariants}
+            className="square"
+          >
+            <img src={Treeguy} alt="Tree guy" className="-z-30" />
+          </motion.div>
         </div>
       </div>
     </>
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,7 +1,13 @@
 import Paint from "../assets/paint.svg";
 import Gum from "../assets/gumbubble.svg";
 import Vegalia from "../assets/vegaliapic.png";
+import { useViewportScroll, motion, useTransform } from "framer-motion";
+
 function Hero() {
+  const { scrollY } = useViewportScroll();
+  const y1 = useTransform(scrollY, [0, 600], [0, -150]);
+  const y2 = useTransform(scrollY, [0, 600], [600, -100]);
+
   return (
     <>
       <div className=" grid grid-cols-2 divide-x">
@@ -11,14 +17,18 @@ function Hero() {
             With Gumroad, anyone can earn their first dollar online. Just start
             with what you know, see what sticks, and get paid. It’s that easy
           </p>
-          <button className="w-[270px] p-[22px] bg-black text-white text-2xl">
+          <button className="w-[270px] p-[22px] bg-black text-white text-2xl hover:-translate-x-2 hover:-translate-y-2 hover:border-r-8 hover:border-b-8 border-yellow-400  duration-200">
             start selling
           </button>
         </div>
         <div className="bg-[#ffc900] p-24 border-l-2 border-b-2 border-black">
           <div>
-            <img src={Gum} alt="gum" className="absolute -bottom-64" />
-            <img src={Paint} alt="paint" className="absolute right-16" />
+            <motion.div className="box" style={{ y: y2, x: 0 }}>
+              <img src={Gum} alt="gum" className="absolute -bottom-64" />
+            </motion.div>
+            <motion.div className="box" style={{ y: y1, x: 100 }}>
+              <img src={Paint} alt="paint" className="absolute right-16" />
+            </motion.div>
             <img src={Vegalia} alt="girl" />
           </div>
           <div className="translate-y-16 -translate-x-12">
